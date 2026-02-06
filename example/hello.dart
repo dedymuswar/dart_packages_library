6ab3081aa2b781db7efea728bdf0ce3cf1683509
dedymuswar <dedymuswar@gmail.com>
2026-02-06 10:38:52 +0900

    dart library package

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:dart_packages_library/hello.dart';
+
+void main(List<String> args) {
+  print(sayHello("Dedy"));
+
+  var customer = Customer();
+  var category = Category();
+  var product = Product();  
+}
